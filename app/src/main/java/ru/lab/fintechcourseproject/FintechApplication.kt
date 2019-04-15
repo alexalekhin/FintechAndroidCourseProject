85b3b673275025110d02263f21c3cc289675a2ac
alex <alehin.av@edu.spbstu.ru>
2019-04-15 08:08:00 +0300

    Basic project setup
    -Application file setup
    -code style files
    -libraries
    -other small things

---
+++ app/src/main/java/ru/lab/fintechcourseproject/FintechApplication.kt
@@ -0,0 +1,18 @@
+package ru.lab.fintechcourseproject
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class FintechApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+        // Normal app init code...
+    }
+}
